043e5fe5a2b930088a13b4d3aee529fde403939f
berdnne <90113754+berdnne@users.noreply.github.com>
2024-12-04 19:48:45 -0600

    Completed functionality

--- main.py
+++ main.py
@@ -9,22 +9,5 @@ def main():
     Gui(window)
     window.mainloop()
 
-    #john_votes = 0
-    #jane_votes = 0
-
-    #choice = vote_menu()
-    #while not(choice == 'x'):
-    #    vote = candidate_menu()
-    #    if vote == 1:
-    #        print('Voted John')
-    #        john_votes += 1
-    #    else:
-    #        print('Voted Jane')
-    #        jane_votes += 1
-
-    #    choice = vote_menu()
-
-    #print('------------------------------\nJohn - ', john_votes, ', Jane - ', jane_votes, ', Total - ', (john_votes + jane_votes), '\n------------------------------', sep = '')
-
 if __name__ == '__main__':
     main()
